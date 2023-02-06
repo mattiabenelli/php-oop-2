4daac11710dce390be88be7e509edac07c5f85c9
mattiabenelli <benellimattia0@gmail.com>
2023-02-06 16:20:02 +0100

    first commit

---
+++ database.php
@@ -0,0 +1,10 @@
+<?php 
+
+include __DIR__ .'/models/AllProducts.php'
+include __DIR__ .'/models/AnimalFood.php'
+include __DIR__ .'/models/AnimalGame.php'
+include __DIR__ .'/models/AnimalProducts.php'
+
+
+
+?>
